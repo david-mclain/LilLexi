c1f0fdaaa47087fb8c5606a947b432936aff304b
David McLain <davidmclain98@gmail.com>
2022-09-30 12:16:16 -0700

    Testing

---
+++ LexiDriver.java
@@ -0,0 +1,11 @@
+public class LexiDriver {
+	private static LilLexiDocument doc = null;
+	public static void main(String[] args) {
+		/*doc = new LilLexiDocument();
+		LilLexiUI UI = new LilLexiUI();
+		LilLexiControl control = new LilLexiControl(doc);
+		UI.setController(control);
+		UI.start();*/
+		new TextEditor();
+	}
+}
