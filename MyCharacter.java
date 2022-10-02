0a4c9a8005f4f196a6febe8a5f8b9b90bbc31dca
David McLain <davidmclain98@gmail.com>
2022-10-01 18:07:41 -0700

    Fixed some issues from pulling and pushing errors

--- MyCharacter.java
+++ MyCharacter.java
@@ -1,21 +1,16 @@
-<<<<<<< HEAD
-=======
-package LilLexi;
-
->>>>>>> fcc2eaacef9d0f92e5194111c9ce1cafce465ff4
 public class MyCharacter extends Glyph {
 	private char myChar;
 	private int row, col, hei, wid;
 
 	
-<<<<<<< HEAD
+
 	public MyCharacter(char c) {
 		// Default values
 		this.myChar = c;
-=======
+	}
+
 	public MyCharacter() {
 		// Default values
->>>>>>> fcc2eaacef9d0f92e5194111c9ce1cafce465ff4
 		row = -1;
 		col = -1;
 		hei = -1;
@@ -56,9 +51,6 @@ public class MyCharacter extends Glyph {
 	@Override
 	public void setBounds(int hei, int wid) { this.hei = hei; this.wid = wid; }
 	
-	
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> fcc2eaacef9d0f92e5194111c9ce1cafce465ff4
+
